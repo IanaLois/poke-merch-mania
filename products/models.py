f8c98e8faa3d6df1af84f62eece7ee148e058934
IanaLois <>
2023-11-27 16:11:36 +0000

    add products app, images, and models

---
+++ products/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    #rating = models.ForeignKey('Rating', null=True, blank=True, on_delete=models.SET_NULL)
+    #collection = models.ManyToManyField('Collection', blank=True)
+    #pokemon = models.ForeignKey('Pokemon', null=True, blank=True, on_delete=models.SET_NULL)
+    image = models.ImageField(upload_to='product_images/', null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    has_sizes = models.BooleanField(default=False)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    
+    def __str__(self):
+        return self.name
